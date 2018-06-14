335da4c22929156db64da7dacb2031496a212249
frankcao@me.com Cao <frankcao@me.com>
2018-06-14 19:21:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,26 +27,8 @@ function johnLennonFacts(arr) {
   }
   return newFacts;
 }
-johnLennonFacts(facts); 
+//johnLennonFacts(facts); 
 
-// var facts = [
-//   "He was the last Beatle to learn to drive",
-//   "He was never a vegetarian",
-//   "He was a choir boy and boy scout",
-//   "He hated the sound of his own voice"
-// ];
-
-// function johnLennonFacts(arr) {
-//   var result= []; 
-//   var i = 0; 
-//   while(i < arr.length) {
-//     result.push(arr[i] + '!!!')
-//     i++; 
-//   }
-//   return result; 
-// }
-
-// johnLennonFacts(facts); 
 
 function iLoveTheBeatles(i) {
   var myLove = [];
