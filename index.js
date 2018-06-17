4faa1f3660240be580340b36c9c288f40e0b1fe5
Frank Cao <frankcao@me.com>
2018-06-17 10:33:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function theBeatlesPlay(musicians, instruments) {
-  const arr = []
+  var arr = []
 
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (var i = 0, l = musicians.length; i < l; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i])
   }
 
   return arr
